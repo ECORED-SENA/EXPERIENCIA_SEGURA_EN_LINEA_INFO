b388e7cced511aef8a37d18cba53beda7ef8b5db
luisEfe80 <luismendez80@hotmail.com>
2021-10-06 12:33:40 -0400

    Ajuste de créditos

--- src/config/global.js
+++ src/config/global.js
@@ -98,7 +98,7 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Emilsen Alfonso Bautista',
         cargo: 'Validación de diseño y contenido',
         centro:
           'Centro Industrial del Diseño y la Manufactura Regional Santander',
